19e7437b7edf46a9a5887281ba03b2472743f147
can <canbolukbas98@gmail.com>
2020-12-29 14:39:30 +0300

    single_url query infos are printed out

--- single_url_recommendation.py
+++ single_url_recommendation.py
@@ -1,16 +1,28 @@
-import urllib.request
+import helper
 
 
 # extract the content of the book whose url is given
 # title, author, description, urls of recommended books and genres
 book_url = "https://www.goodreads.com/book/show/18050143-zero-to-one"
-f = urllib.request.urlopen(book_url)
-myfile = f.read()
-print(myfile)
+title, authors, description, urls_of_recommended_books = helper.parse(book_url)
 
 # output the content into terminal (I guess)
+print("title: " + title)
+print()
+print("authors:")
+print(", ".join(authors))
+print()
+print("Description")
+print(description)
+print()
+print("urls of recommended books:")
+for url in urls_of_recommended_books:
+    print(url)
+print()
+#print(myfile)
 
 # Encode the content of the book.(vectorise I guess)
+
 
 # Recommend 18 books for this book with the model based on cosine similarity.
 # set alpha "reasonable"
